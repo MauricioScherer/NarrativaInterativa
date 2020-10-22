099a0e6b4a2da7e07b7a7e36b72a6b5df280afee
MauricioScherer <mauricio.sscherer@gmail.com>
2020-10-22 19:31:06 -0300

    Mauricio

---
+++ JogoNarrativaInterativa/Assets/Script/LookViewcamera.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LookViewcamera : MonoBehaviour
+{
+    private Transform mainCamera;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        mainCamera = GameObject.FindGameObjectWithTag("MainCamera").GetComponent<Transform>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(gameObject.activeSelf)
+            transform.LookAt(mainCamera);
+    }
+}
